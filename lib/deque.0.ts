0321291b716c117ec3bfd2e680ff88ac5d6a18e8
adminadmin <git@akki.uk>
2025-11-27 21:02:25 +0000

    fix `Deque` growth check off-by-ones, add `Deque.set`, fix `Deque.clone` creating a dynamic array. add `DequeArrayLike` and `DequeArrayLike2` `Deque` subclasses.

--- lib/deque.0.ts
+++ lib/deque.0.ts
@@ -1,14 +1,20 @@
+import { EventEmitter } from 'node:events';
 import { inspect } from 'node:util';
 
 // write then increment for pushBack, decrement then read for popBack
 // decrement then write for pushFront, read then increment for popFront
 // i.e. this.#back = writeable ix, this.#front = readable ix
 
+const ARRAYLIKE_PROPERTY_RE = /-?[0-9+]/u;
+
+type ArrayLike<Item> = Record<number, Item> & { length: number };
+
 export enum DequeDelete {
   None,
   Null,
   Full,
 }
+
 export interface DequeParams {
   growthFactor?: number | false;
   deleteStrategy?: DequeDelete;
@@ -25,7 +31,9 @@ export class Deque<Item> {
   #growthFactor: number | false;
   #deleteStrategy: DequeDelete;
   #startingLength: number;
-  #size = 0;
+  #size: number;
+  // used by `DequeArrayLike2`
+  protected emitter = new EventEmitter<{ grow: [from: number, to: number] }>();
   /** Lower growth factor is better for ram but higher is better for performance
    *
    * Full delete after pop allows gc but hurts performance. Leaving popped value untouched is fastest but prevents gc until it's overwritten. Setting popped value to null has minimal performance impact and allows gc
@@ -36,17 +44,23 @@ export class Deque<Item> {
     if (typeof growthFactor === 'number' && growthFactor <= 1) throw new Error('growthFactor must be false or >1');
     this.#growthFactor = growthFactor;
     this.#deleteStrategy = deleteStrategy;
-    if (typeof param === 'number') this.#array = new Array(Math.max(param, 1));
-    else {
+    if (typeof param === 'number') {
+      this.#array = new Array(Math.max(param, 1));
+      this.#size = 0;
+    } else {
       const items = [...param];
       this.#array = new Array(Math.ceil(Math.max(items.length * (this.#growthFactor ? this.#growthFactor : 1), 1)));
       // push back
       for (const item of items) this.#array[this.#back++] = item;
+      this.#size = items.length;
     }
     this.#startingLength = this.#array.length;
   }
   #grow(): boolean {
     if (!this.#growthFactor) return false;
+    // it's significantly faster to create a new array and copy everything over than to expand the existing array by writing beyond the last ix, then moving everything from front ix up to the new end
+    // i suppose it must also be reallocating and copying internally
+    const prevLength = this.#array.length;
     const array = new Array(Math.ceil(this.#array.length * this.#growthFactor));
     for (let destIx = 0; destIx < this.#array.length; ++destIx) {
       const intermediate = destIx + this.#back;
@@ -55,6 +69,7 @@ export class Deque<Item> {
     this.#front = 0;
     this.#back = this.#array.length;
     this.#array = array;
+    this.emitter.emit('grow', prevLength, this.#array.length);
     return true;
   }
   pushBack(...values: Item[]) {
@@ -62,7 +77,7 @@ export class Deque<Item> {
       this.#array[this.#back] = value;
       if (this.#back === this.#array.length - 1) this.#back = 0;
       else ++this.#back;
-      if (this.#size < this.#array.length || (this.#growthFactor && this.#grow())) ++this.#size;
+      if (this.#size < this.#array.length - 1 || (this.#growthFactor && this.#grow())) ++this.#size;
       // array is full and growth is disabled
       else if (this.#front === this.#array.length - 1) this.#front = 0;
       else ++this.#front;
@@ -74,7 +89,7 @@ export class Deque<Item> {
       if (this.#front === 0) this.#front = this.#array.length - 1;
       else --this.#front;
       this.#array[this.#front] = value;
-      if (this.#size < this.#array.length || (this.#growthFactor && this.#grow())) ++this.#size;
+      if (this.#size < this.#array.length - 1 || (this.#growthFactor && this.#grow())) ++this.#size;
       // array is full and growth is disabled
       else if (this.#back === 0) this.#back = this.#array.length - 1;
       else --this.#back;
@@ -110,6 +125,16 @@ export class Deque<Item> {
     if (-index > this.#size) return;
     const intermediate = this.#back + index;
     return this.#array.at(intermediate >= 0 ? intermediate : intermediate + this.#array.length);
+  }
+  set(index: number, value: Item): Item {
+    if (index >= 0) {
+      // regular `Array` grows when assigning to an oob index but that would break things
+      if (index >= this.#size) throw new Error('out of bounds');
+      const intermediate = index + this.#front;
+      return this.#array[intermediate < this.#array.length ? intermediate : intermediate - this.#array.length] = value;
+    } else if (-index > this.#size) { throw new Error('out of bounds'); }
+    const intermediate = this.#back + index;
+    return this.#array[intermediate >= 0 ? intermediate : intermediate + this.#array.length] = value;
   }
   includes(value: Item, fromIndex?: number): boolean {
     for (let i = fromIndex ?? 0; i < this.#size; ++i) {
@@ -159,15 +184,7 @@ export class Deque<Item> {
     this.#size = 0;
   }
   clone(): Deque<Item> {
-    const deque = new Deque<Item>();
-    deque.#array = [...this.#array];
-    deque.#front = this.#front;
-    deque.#back = this.#back;
-    deque.#size = this.#size;
-    deque.#startingLength = this.#startingLength;
-    deque.#growthFactor = this.#growthFactor;
-    deque.#deleteStrategy = this.#deleteStrategy;
-    return deque;
+    return new Deque(this, { deleteStrategy: this.#deleteStrategy, growthFactor: this.#growthFactor });
   }
   *itemsFront(): Generator<Item, void, void> {
     for (let i = 0; i < this.#size; ++i) {
@@ -196,6 +213,9 @@ export class Deque<Item> {
   get size() {
     return this.#size;
   }
+  protected get internalLength() {
+    return this.#array.length;
+  }
   [Symbol.iterator]() {
     return this.itemsFront();
   }
@@ -207,5 +227,91 @@ export class Deque<Item> {
       length: this.#array.length,
       size: this.#size,
     };
+  }
+}
+
+/** `Deque` but you can also get and set members by their index in square bracket notation like an `Array`
+ *
+ * e.g., `new Deque(['a', 'b' ,'c'])[1]` returns `b`
+ *
+ * **This requires proxying which adds overhead to every single method call and property access** */
+export class DequeArrayLike<Item> extends Deque<Item> implements ArrayLike<Item> {
+  // This is just for typing and is overriden by the Proxy returned by the constructor since the `property in target` check fails
+  [index: number]: Item;
+  constructor(...params: ConstructorParameters<typeof Deque<Item>>) {
+    super(...params);
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+    return new Proxy(this, {
+      get(target, property, _receiver) {
+        if (property in target) {
+          const getter = target[property as keyof typeof target];
+          if (getter instanceof Function) {
+            // this is faster and seems to work reliably
+            return getter.bind(target);
+            // return function (...args: unknown[]) {
+            //   /** > It's important to remember that TypeScript doesn't change the runtime behavior of JavaScript, and that JavaScript is somewhat famous for having some peculiar runtime behaviors.
+            //    *  >
+            //    *  > JavaScript's handling of `this` is indeed unusual
+            //    *  *~ [Orta, 2020](https://github.com/microsoft/TypeScript-Website/blame/5b2c0a7c557102f2b6aa1c736fbea94e9863d82e/packages/documentation/copy/en/handbook-v2/Classes.md#L979)* */
+            //   // @ts-expect-error ugh
+            //   return getter.apply(this === receiver ? target : this, args);
+            // };
+          }
+          return getter;
+        }
+        if (typeof property === 'string' && ARRAYLIKE_PROPERTY_RE.exec(property))
+          return target.at(parseInt(property, 10));
+      },
+      set(target, property, value, receiver) {
+        if (property in target) {
+          const setter = target[property as keyof typeof target];
+          // @ts-expect-error also ugh
+          setter.apply(this === receiver ? target : this)(value);
+          return true;
+        }
+        if (typeof property === 'string' && ARRAYLIKE_PROPERTY_RE.exec(property)) {
+          target.set(parseInt(property, 10), value);
+          return true;
+        }
+        return false;
+      },
+    });
+  }
+  // ArrayLike also needs to implement `length`
+  get length(): number {
+    return this.size;
+  }
+}
+
+/** `Deque` but you can also get and set members by their index in square bracket notation like an `Array`
+ *
+ * e.g., `new Deque(['a', 'b' ,'c'])[1]` returns `b`
+ *
+ * **This requires defining properties for every possible new array index each time the internal array grows which is very slow**
+ *
+ * This also seems to add some overhead to other method calls */
+export class DequeArrayLike2<Item> extends Deque<Item> implements ArrayLike<Item> {
+  // this is overriden by #createProperties
+  [index: number]: Item;
+  #createProperties(from: number, to: number) {
+    for (let i = from; i < to; ++i) {
+      Object.defineProperty(this, i, {
+        get() {
+          return this.at(i);
+        },
+        set(value: Item) {
+          return this.set(i, value);
+        },
+      });
+    }
+  }
+  constructor(...params: ConstructorParameters<typeof Deque<Item>>) {
+    super(...params);
+    this.#createProperties(0, this.internalLength);
+    this.emitter.addListener('grow', this.#createProperties);
+  }
+  // ArrayLike also needs to implement `length`
+  get length(): number {
+    return this.size;
   }
 }
